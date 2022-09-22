c1bcf214f83ca69acdb92d6e093bde095daf5e96
Cindy <51303237+happycindy26@users.noreply.github.com>
2022-09-22 12:21:22 +1000

    add navlink on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 import { Button } from "react-bootstrap"
 
 function Home() {
@@ -5,7 +6,9 @@ function Home() {
     <div className="vh-100"
       style={{backgroundImage: "url(imgs/full.jpg)"}}
     >
-      <Button href="/store" variant="outline-primary" className="m-5 fs-2" style={{fontFamily: "Niconne", borderRadius: "5rem"}}>Shop now</Button>
+      <NavLink to="/store">
+        <Button variant="outline-primary" className="m-5 fs-2" style={{fontFamily: "Niconne", borderRadius: "5rem"}}>Shop now</Button>
+      </NavLink>
     </div>
   )
 }
