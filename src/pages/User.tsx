f238ed8eddec4f42b7e0664cb44e10247f61b116
Cindy <51303237+happycindy26@users.noreply.github.com>
2022-11-14 20:32:37 +1100

    center user auth text

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -3,15 +3,17 @@ import '@aws-amplify/ui-react/styles.css';
 
 const User = () => {
   return (
+    
     <Authenticator>
       {({signOut}) => (
-        <div>
+        <div className="mt-5 text-center">
           <h1>Hello</h1>
           <h3>You are authenticated!</h3>
           <button onClick={signOut}>Sign out</button>
         </div>
       )}
     </Authenticator>
+   
   )
 }
 
