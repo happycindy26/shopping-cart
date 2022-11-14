1bca755e25cc2adb4f2b80685f3c00ef0194a81b
Cindy <51303237+happycindy26@users.noreply.github.com>
2022-11-14 20:50:18 +1100

    center amplify auth text

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,19 +1,18 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import { View } from '@aws-amplify/ui-react';
 
 const User = () => {
   return (
-    
     <Authenticator>
       {({signOut}) => (
-        <div className="mt-5 text-center">
+        <View textAlign="center">
           <h1>Hello</h1>
           <h3>You are authenticated!</h3>
           <button onClick={signOut}>Sign out</button>
-        </div>
+        </View>
       )}
     </Authenticator>
-   
   )
 }
 
